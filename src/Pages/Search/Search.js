921ad935100f96c717dba422708909be41fd284d
nidheedpanchal05 <nidheedpanchal@gmail.com>
2021-02-22 18:11:12 +0530

    First Commit - UI design

---
+++ src/Pages/Search/Search.js
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <div>
+            <span className="pageTitle">Search</span>
+            <input style={{display:"block", position:"relative", top:"80px", left:"460px", height:'30px',width:'300px' }} type="text" />
+        
+        </div>
+    )
+}
+export default Search;
