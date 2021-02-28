83d993368c281c4054170c9a409dd09c2b3c2c4c
nidheedpanchal05 <nidheedpanchal@gmail.com>
2021-02-28 12:00:19 +0530

    Second commit - pages, components and genre

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,7 @@
 const Search = () => {
     return (
         <div>
-            <span className="pageTitle">Search</span>
-            <input style={{display:"block", position:"relative", top:"80px", left:"460px", height:'30px',width:'300px' }} type="text" />
-        
+            <span className="pageTitle">Search</span>       
         </div>
     )
 }
